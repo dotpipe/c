024487238edf702592d80508b38e110dc3142f77
wise-penny <inland14@live.com>
2023-03-25 13:55:21 -0400

    Add files via upload

--- press.cpp
+++ press.cpp
@@ -9,46 +9,27 @@
 
 using namespace std;
 
-void retrieve(string buf, ofstream& out)
+string retrieve(string& c, ofstream& out)
 {
-	uint8_t b_bit = 0;
-	string cp = "";
-	uint8_t sp = 0;
-	uint8_t i = 8, j = 5, i_cnt = 0, q = 0, i_back_cnt = 0;
-	for (uint8_t x : buf)
+	uint8_t i_cnt = 0;
+	string section_str = "";
+	while (1 < c.length())
 	{
-		if (b_bit%2 == 0)
+		uint64_t space_bin = bitset<8>(c.front()).to_ulong();
+		c = c.substr(1);
+		while (space_bin > 4)
 		{
-			sp = (x-4);
-			b_bit++;
-			continue;
+			section_str += (char)(0);
+			space_bin--;
 		}
-		else
-		{
-			cp.append((sp*8),'0');
-			cp.append(bitset<64>(x).to_string().substr(0,i%(cp.length()+1)));
-		}
-		q = 0;
-		while (q++ < 8)
-		{
-			out << (uint8_t)bitset<8>(cp.substr(0,8%(cp.length()+1))).to_ullong();
-			cp = cp.substr(8%(cp.length()+1));
-		}
-		if (cp.length() > 0)
-			out << (uint8_t)bitset<8>(cp).to_ullong();
-		int t = j;
-		j += i;
-		i = t;
-		if (i > 31)
-		{
-			i = 8;
-			j = 5;
-		}
-		b_bit++;
+		section_str += (uint8_t)c.front();
+		c = c.substr(1);
 	}
+	
+	return section_str;
 }
 
-void recollect(ifstream& in, ofstream& out) {
+bool recollect(ifstream& in) {
 
 	stringstream rd;
 	rd << in.rdbuf();
@@ -56,11 +37,30 @@ void recollect(ifstream& in, ofstream& out) {
 	string buf = rd.str();
 	rd.str("");
 	uint64_t buf_len = buf.length(), bin_len = 0, buf_left = buf_len;
-	while (16 < buf.length())
+	uint8_t fn_s = buf.find("START---"); // 8 chars long
+	uint8_t fn_t = buf.find("-----------------S",18 + fn_s + 8);
+	string full_size_str = buf.substr(18, fn_s-18);
+	buf = buf.substr(fn_s+8);
+	uint64_t full_size = 0;
+	cout << full_size_str << " " << flush;
+	uint8_t fss = full_size_str.length();
+	for (int8_t i = 1 ; i < fss ; i++)
 	{
-		retrieve(buf.substr(0,16), out);
-		buf = (buf.length() >= 16) ? buf.substr(16) : "";
+		full_size += (10*i) * (int)(full_size_str[i-1] - 48);
 	}
+	fn_t = buf.find("-----------------S");
+	uint32_t filename_len = buf.find("-----------------S");
+	string filename_str = buf.substr(0,fn_t);
+	cout << filename_str << " " << flush;
+	ofstream out {filename_str.c_str(), std::ios_base::out | std::ios_base::trunc };
+	buf = buf.substr(18+fss+fn_t-6);
+	uint64_t eOf = buf.find("-----------------END--------------------S");
+	if (eOf != string::npos)
+		buf = buf.substr(0,eOf);
+	cout << "$$$$$$$" << buf.substr(eOf) << flush;
+	string return_str = retrieve(buf, out);
+	out.write(return_str.c_str(),return_str.length());
+	return (buf.length() > 0);
 } 
 
 void collect(ifstream& in, ofstream& out, string outfilename) {
@@ -74,7 +74,7 @@ void collect(ifstream& in, ofstream& out, string outfilename) {
 	uint64_t buf_left = buf_len;
 	string bin = "";
 	uint32_t buf_str_len = 6400000;
-	const int MAX_BITS = 1024;
+	const int MAX_BITS = 800;
 	out << "-----------------S" << buf.length() << "START---" << outfilename << "-----------------S";
 	for ( ; 0 < buf.length() ; ) {
 
@@ -89,77 +89,87 @@ void collect(ifstream& in, ofstream& out, string outfilename) {
 			{
 				c.append(bitset<8>(x).to_string());
 			}
-			int8_t i = 8, j = 5, i_cnt = 4, q = 0;
+			uint64_t i = 64, j_cnt = 1, h_cnt = 1, k_cnt = 1, i_cnt = 1, l_cnt = 1, q = 0;
 			uint64_t ch = 0;
 			while (c.length() > 0)
 			{
-				
-				if (i > (c.length()))
+				if (i >= (c.length()))
 					i = c.length();
-				ch = bitset<64>(c.substr(c.length()-i)).to_ullong();
+				// if (i > 64)
+				// 	i = 8;
+				ch = bitset<64>(c.substr(0,i%(c.length()+1))).to_ulong(); //(c.length()-(i%(c.length()+1)))).to_ullong();
 				while (ch > 0)
 				{
-					if (ch%128 == 0)
+					if (ch%32 >= 16)//ch%8 == 7 || ch%8 == 6)
 					{
-						i_cnt++;
-						string bin_temp((char *)(128));
-						while (i_cnt >= 128 && i_cnt - 128 > 0)
+						ch -= 16;
+						l_cnt <<= 4;
+						l_cnt += (ch%32);
+						ch >>= 5;
+					}
+					if (ch%32 >= 8)//ch%8 == 7 || ch%8 == 6)
+					{
+						ch -= 8;
+						h_cnt <<= 3;
+						h_cnt += (ch%32);
+						ch >>= 5;
+					}
+					if (ch%32 >= 4)//ch%8 == 7 || ch%8 == 6)
+					{
+						ch -= 4;
+						i_cnt <<= 2;
+						i_cnt += (ch%32);
+						ch >>= 5;
+					}
+					if (ch%32 >= 2)//ch%8 == 7 || ch%8 == 6)
+					{
+						ch -= 2;
+						j_cnt <<= 1;
+						j_cnt += (ch%32);
+						ch >>= 5;
+					}
+					if (ch%32 >= 0)//ch%8 == 7 || ch%8 == 6)
+					{
+						k_cnt <<= 1;
+						k_cnt += (ch%32);
+						ch >>= 5;
+					}
+					if (j_cnt > pow(2,62) || h_cnt > pow(2,62) || i_cnt > pow(2,62) || k_cnt > pow(2,62))
+					{	
+						h_cnt >>= 1;
+						i_cnt >>= 1;
+						j_cnt >>= 1;
+						k_cnt >>= 1;
+						l_cnt >>= 1;
+						while (k_cnt > 1 || i_cnt > 1 || h_cnt > 1 || j_cnt > 1)
 						{
-							bin_temp += (uint8_t)((128 - i_cnt)%128);
-							i_cnt >>= 7;
+							bin += (char)((h_cnt%256)) + (char)((i_cnt%256)) + (char)(j_cnt%256) + (char)(k_cnt%256) + (char)(l_cnt%256);
+							i_cnt >>= 8;
+							j_cnt >>= 8;
+							h_cnt >>= 8;
+							k_cnt >>= 8;
 						}
-						if (bin_temp.length() > 1)
-							bin += (uint8_t)(i_cnt);
+						l_cnt = i_cnt = h_cnt = k_cnt = j_cnt = 1;
 					}
-					else if (i_cnt == 4)
-					{
-						bin += (uint8_t)(129) + (uint8_t)(ch%128);
-					}
-					else if (i_cnt > 4)
-					{
-						bin += (uint8_t)(130) + (uint8_t)(i_cnt) + (uint8_t)(ch%128);
-						i_cnt = 4;
-					}
-					// else
-					// {
-					// 	bin += (uint8_t)(131) + (uint8_t)(i_cnt) + (uint8_t)(ch%128);
-					// 	i_cnt = 4;
-					// }
-					ch >>= 7;
 				}
-				c = c.substr(0,c.length()-i);
-				int t = j;
-				j += i;
-				i = t;
-				if (i >= (c.length()))
-					c = "";
+				while (k_cnt > 1 || i_cnt > 1 || h_cnt > 1 || j_cnt > 1)
+				{
+					bin += (char)((h_cnt%256)) + (char)((i_cnt%256)) + (char)(j_cnt%256) + (char)(k_cnt%256) + (char)(l_cnt%256);
+					i_cnt >>= 8;
+					j_cnt >>= 8;
+					h_cnt >>= 8;
+					k_cnt >>= 8;
+				}
+				c = c.substr(i%(c.length()+1)); // c.length()-(i%(c.length()+1)));
+				// i += 64;
 			}
-			while (ch > 0)
+			if (i_cnt > 4)
 			{
-				if (ch%128 == 0)
-				{
-					i_cnt++;
-					string bin_temp((char *)(128));
-					while (i_cnt >= 128 && i_cnt - 128 > 0)
-					{
-						bin_temp += (uint8_t)((128 - i_cnt)%128);
-						i_cnt >>= 7;
-					}
-					if (bin_temp.length() > 1)
-						bin += bin_temp + (char*)(i_cnt);
-				}
-				else if (i_cnt == 4)
-				{
-					bin += (uint8_t)(129) + (uint8_t)(ch%128);
-				}
-				else if (i_cnt > 4)
-				{
-					bin += (uint8_t)(130) + (uint8_t)(i_cnt) + (uint8_t)(ch%128);
-					i_cnt = 4;
-				}
-				ch >>= 7;
+				bin += (char)(i_cnt) + (char)(ch%256);
+				i_cnt = 0;
+				ch >>= 8;
 			}
-			if (bin.length() > 1000000)
+			if (bin.length() > 100000)
 			{
 				out << bin;
 				bin_len += bin.length();
@@ -176,7 +186,7 @@ void collect(ifstream& in, ofstream& out, string outfilename) {
 	cout << "[ [ Left:Out 0:" << bin_len << " | " << (bin_len/(double)(buf_len - buf_left)*100) << "% | " << (((buf_len-buf_left)/(double)(buf_len))*100) << "% ]  ]\t\t\r" << flush;
 	bin_len += bin.length();
 	out << bin;
-	out << "-----------------S" << bin_len << "END---" << outfilename << "-----------------S";
+	out << "-----------------END--------------------S";
 	return;
 }
 
@@ -187,7 +197,7 @@ int main(int argc, char *argv[]) {
 	vector<ofstream> ofstreams;
 	string fname = "";
 
-    	printf("Press, Copyright Aunk 2016\n\r: ? to exit - : ! to go back\r\n");
+    	printf("Press, Copyright Aunk 2016\n\r: ? to continue - : ! to go back\r\n");
 
 
 	std::setlocale(LC_ALL, "en_US-UTF8");
@@ -235,19 +245,14 @@ int main(int argc, char *argv[]) {
 
 
 	if (string(argv[1]) == "-d") {
-		printf("\nOutput File: ");
+		printf("\nInput File: ");
 		
 		cin >> fname;
 		
 		ifstream in {fname.c_str(), std::ios_base::in | std::ios_base::binary };
-
-		cin >> fname;
-
-		ofstream out {fname.c_str(), std::ios_base::out | std::ios_base::trunc };
-
 		cout << "Data sorting.. [" << flush; 
 
-		recollect(in,out);
+		(recollect(in));
 
 	}
 	
